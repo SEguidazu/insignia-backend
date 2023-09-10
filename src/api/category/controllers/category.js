124f90994f276a674cb3ed2128b0c7303ff9b57e
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-09-10 01:29:16 -0300

    CATEGORY: use sanitizeQuery in controller

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -1,9 +1,22 @@
-'use strict';
+// @ts-nocheck
+"use strict";
 
 /**
  * category controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController(
+  "api::category.category",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+      const response = await strapi
+        .service("api::category.category")
+        .find(sanitizedQueryParams);
+
+      return response;
+    },
+  })
+);
