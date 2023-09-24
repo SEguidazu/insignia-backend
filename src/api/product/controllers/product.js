bae475fcc4d4663173c90549b0bd4efde5a5fe91
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-09-24 02:29:47 -0300

    FEATUREDPROD: add new attribute to products

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,9 +1,19 @@
-'use strict';
+// @ts-nocheck
+"use strict";
 
 /**
  * product controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController("api::product.product", ({ strapi }) => ({
+  async find(ctx) {
+    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    const response = await strapi
+      .service("api::product.product")
+      .find(sanitizedQueryParams);
+
+    return response;
+  },
+}));
