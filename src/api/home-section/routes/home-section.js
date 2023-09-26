bf040014e4a3390e8507d28bcc0960d28d71ce71
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-09-26 01:31:29 -0300

    clean unused schemas

--- src/api/home-section/routes/home-section.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-section router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::home-section.home-section');
--- src/api/home-section/services/home-section.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-section service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::home-section.home-section');
