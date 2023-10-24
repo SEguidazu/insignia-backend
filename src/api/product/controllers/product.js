2cf9fb6707c7bae80c760fb8f08a81bebf110403
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-10-24 02:10:11 -0300

    update way to validate and sanitize query params in product controller

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,10 +6,16 @@
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
+const { sanitize, validate } = require("@strapi/utils");
 
 module.exports = createCoreController("api::product.product", ({ strapi }) => ({
   async find(ctx) {
-    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    const contentType = strapi.contentType("api::product.product");
+    await validate.contentAPI.query(ctx.query, contentType);
+    const sanitizedQueryParams = await sanitize.contentAPI.query(
+      ctx.query,
+      contentType
+    );
     const response = await strapi
       .service("api::product.product")
       .find(sanitizedQueryParams);
