c876e0b521199aa334a480adaca081ba053ae301
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-10-26 00:29:36 -0300

    update subcategory controller

--- src/api/subcategory/controllers/subcategory.js
+++ src/api/subcategory/controllers/subcategory.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * subcategory controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::subcategory.subcategory');
+module.exports = createCoreController(
+  "api::subcategory.subcategory",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+      const response = await strapi
+        .service("api::subcategory.subcategory")
+        .find(sanitizedQueryParams);
+
+      return response;
+    },
+  })
+);
