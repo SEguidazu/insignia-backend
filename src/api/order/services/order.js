642b9a3044ffc4706381a7fae00ba2b9d24f39a5
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-02-24 00:42:43 -0300

    adds the functionality to subtract the quantity from the stock of the product in the create method of orders

--- src/api/order/services/order.js
+++ src/api/order/services/order.js
@@ -1,9 +1,37 @@
-'use strict';
+"use strict";
 
 /**
  * order service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::order.order');
+module.exports = createCoreService("api::order.order", ({ strapi }) => ({
+  async create(params) {
+    const products = params.data?.order_qty;
+    // @ts-ignore
+    const result = await super.create(params);
+
+    try {
+      products?.forEach(async (item) => {
+        const entry = await strapi.entityService.findOne(
+          "api::product.product",
+          item.id,
+          {
+            fields: ["stock"],
+          }
+        );
+
+        await strapi.entityService.update("api::product.product", item.id, {
+          data: {
+            stock: entry.stock - item.quantity,
+          },
+        });
+      });
+    } catch (error) {
+      console.error("[UPDATE_STOCK_CREATE_ORDER]:", error);
+    }
+
+    return result;
+  },
+}));
