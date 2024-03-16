1bb56c5961e8d4f723792b89fb56969f36ae5bb6
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-03-15 23:59:51 -0300

    prettify type response in controller

--- src/api/type/controllers/type.js
+++ src/api/type/controllers/type.js
@@ -1,9 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * type controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
+const { sanitize, validateYupSchema } = require("@strapi/utils");
 
-module.exports = createCoreController('api::type.type');
+module.exports = createCoreController("api::type.type", ({ strapi }) => ({
+  async find(ctx) {
+    const contentType = strapi.contentType("api::type.type");
+    await validateYupSchema(contentType, ctx.query);
+    const sanitizedQueryParams = await sanitize.contentAPI.query(
+      ctx.query,
+      contentType
+    );
+    const response = await strapi
+      .service("api::type.type")
+      .find(sanitizedQueryParams);
+
+    return response;
+  },
+}));
